c5b111384676d656fddb352dec1a4aa8d36b3653
eager-strategy-8796 <cheniasli@gmail.com>
2019-06-05 14:53:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function add2(n) {
   // Feel free to move things around!
  
 }
+var theFunk = funkyFunction()
 
 var funkyFunction = function() {
   return function() {
@@ -28,4 +29,3 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()
