b2d2dbe827bf4a28e216c88d2ed338264a2b3492
eager-strategy-8796 <cheniasli@gmail.com>
2019-06-05 15:04:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var animal = 'dog'
+var animal = 'dog';
 
 function myAnimal() {
-  return animal
+  return animal;
 }
 
 function yourAnimal() {
@@ -9,12 +9,12 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   animal = 'cat';
-  return animal
+  return animal;
 }
 
 function add2(n) {
-  const two = 2
-  return n + two
+  const two = 2;
+  return n + two;
 
   // Feel free to move things around!
  
@@ -22,10 +22,10 @@ function add2(n) {
 
 var funkyFunction = function() {
   return function() {
-    return "FUNKY!"
-  }
-}
+    return "FUNKY!";
+  };
+};
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction(());
